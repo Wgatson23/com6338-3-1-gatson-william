1b4902472726849628fc71c78fb2599543e199ff
William <wgatson@ufl.edu>
2024-01-29 18:29:02 -0500

    Final, I think

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
-function startQuiz() {
-	var questionsArr = [
-        {
-            question: "Is the sky blue?",
-            answer: true,
-        },
-        {
-            question: "Is the grass green?",
-            answer: true,
-        },
-        {
-            question: "Is the ocean purple?",
-            answer: false,
-        },
-        {
-            question: "Is the sun hot?",
-            answer: true,
-        },
-        {
-            question: "Is the moon made of cheese?",
-            answer: false,
-        },
-    ];
+var questionsArr = [
+    {
+        question: "Go Gators!",
+        answer: true,
+    },
+    {
+        question: "Starbucks has the best coffee?",
+        answer: true,
+    },
+    {
+        question: "Pineapples belong on pizza?",
+        answer: true,
+    },
+    {
+        question: "Are you a robot?",
+        answer: false,
+    },
+    {
+        question: "Are you going to pass this quiz?",
+        answer: true,
+    },
+];
+
+function runQuiz() {
     var score = 0;
+    var total = questionsArr.length;
     for (var i = 0; i < questionsArr.length; i++) {
-        var answer = confirm(questionsArr[i].question);
-        if (answer === questionsArr[i].answer) {
+        var response = window.confirm(questionsArr[i].question);
+        if (response == questionsArr[i].answer) {
             score++;
+        } else {
         }
     }
-    alert("Your score is " + score + "/" + questionsArr.length);
+    var finalScore = 100 * (score / total);
+    alert("your score is  " + Math.round(finalScore) + "%");
 }
+
